b51a67c68b9b73ff1ad8c4774bcbd33ce209fe1e
rackevitamas <90187820+rackevitamas@users.noreply.github.com>
2024-02-20 14:12:46 +0100

    Add files via upload

--- Matek.java
+++ Matek.java
@@ -14,19 +14,19 @@ public class Matek {
         double ceily = Math.ceil(y);
         double floorx = Math.floor(x);
         double floory = Math.floor(y);
-        System.out.print("A két szám közül nagyobb szám: " + x + " " + y + " " + max);
-        System.out.print("\nA két szám közül kisebb szám: " + x + " " + y + " " + min);
-        System.out.print("\nAz x szám négyzetgyök szám: " + x  + " " + gyokx);
-        System.out.print("\nAz y szám négyzetgyök szám: " + y + " " + gyoky);
-        System.out.print("\nA két szám különsbég szám: " + x + " - " + y + " " + (x - y));
-        System.out.print("\nAz x szám köbe szám: " + x + " " + powx);
-        System.out.print("\nAz y szám köbe szám: " + y + " " + powy);
-        System.out.print("\nAz x szám kerekítés szám: " + x + " " + roundx);
-        System.out.print("\nAz y szám kerekítés szám: " + y + " " + roundy);
-        System.out.print("\nAz x szám felfelé kerekítés szám: " + x + " " + ceilx);
-        System.out.print("\nAz y szám felfelé kerekítés szám: " + y + " " + ceily);
-        System.out.print("\nAz x szám lefelé kerekítés szám: " + x + " "  + floorx);
-        System.out.print("\nAz y szám lefelé kerekítés szám: " + y + " " + floory);
+        System.out.print("A két szám közül nagyobb szám: " + x + ", " + y + ": " + max);
+        System.out.print("\nA két szám közül kisebb szám: " + x + ", " + y + ": " + min);
+        System.out.print("\nAz x szám négyzetgyök szám: " + x  + ", " + gyokx);
+        System.out.print("\nAz y szám négyzetgyök szám: " + y + ", " + gyoky);
+        System.out.print("\nA két szám különsbég szám: " + x + " - " + y + " = " + (x - y));
+        System.out.print("\nAz x szám köbe szám: " + x + ", " + powx);
+        System.out.print("\nAz y szám köbe szám: " + y + ", " + powy);
+        System.out.print("\nAz x szám kerekítés szám: " + x + ", " + roundx);
+        System.out.print("\nAz y szám kerekítés szám: " + y + ", " + roundy);
+        System.out.print("\nAz x szám felfelé kerekítés szám: " + x + ", " + ceilx);
+        System.out.print("\nAz y szám felfelé kerekítés szám: " + y + ", " + ceily);
+        System.out.print("\nAz x szám lefelé kerekítés szám: " + x + ", "  + floorx);
+        System.out.print("\nAz y szám lefelé kerekítés szám: " + y + ", " + floory);
 
     }
 }
